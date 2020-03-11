a29b291d7295dfb91d61e671756d9b634153c6ff
Nadezhda Khimunina <61099100+nkhimunina@users.noreply.github.com>
2020-03-11 17:27:52 +0300

    Fixed equation.js.

--- JavaScript/js/linear/equation.js
+++ JavaScript/js/linear/equation.js
@@ -1,13 +1,16 @@
 // Найти корни квадратного уравнения. (ax2 + bx + c)
 
-let a = 5;
-let b = 10;
-let c = 2;
-
-let D = b*b - 4*a*c;
-
-let X1 = (-b + Math.sqrt(D))/(2*a);
-let X2 = (-b - Math.sqrt(D))/(2*a);
-
+let a = Math.floor(Math.random() * (100 + 100 + 1)) - 100;
+let b = Math.floor(Math.random() * (100 + 100 + 1)) - 100;
+let c = Math.floor(Math.random() * (100 + 100 + 1)) - 100;
 console.log("a = ", a, "b = ", b, "c = ", c);
-console.log("X1 = ", X1, "X2 = ", X2);
+
+let X1, X2;
+let D = b*b - 4*a*c;
+if (D < 0) {
+    console.log("D < 0");
+} else {
+    X1 = (-b + Math.sqrt(D))/(2*a);
+    X2 = (-b - Math.sqrt(D))/(2*a);
+    console.log("X1 =", X1, "X2 =", X2);
+}
