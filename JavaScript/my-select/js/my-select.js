b4a170037520d7f040be03e03b8b011b4ad99ee8
Nadezhda Khimunina <61099100+nykhimun@users.noreply.github.com>
2020-04-03 03:23:10 +0300

    Added filter logic.

--- JavaScript/my-select/js/my-select.js
+++ JavaScript/my-select/js/my-select.js
@@ -1,29 +1,41 @@
 let array = [
-    'Побег из Шоушенка', 
-    'Зеленая миля',
-    'Форрест Гамп', 
-    'Список Шиндлера',
-    '1+1',
-    'Начало',
-    'Леон',
-    'Король Лев',
-    'Бойцовский клуб',
-    'Иван Васильевич меняет профессию',
-    'Жизнь прекрасна',
-    'Достучаться до небес',
-    'Крестный отец',
-    'Криминальное чтиво',
-    'Операция «Ы» и другие приключения Шурика',
-    'Престиж',
-    'Игры разума',
-    'Интерстеллар',
-    'Властелин колец: Возвращение Короля',
-    'Гладиатор',
-    'Назад в будущее',
-    'Карты, деньги, два ствола',
-    'Матрица',
-    'Бриллиантовая рука',
-    'Отступники'
+    'Бангладеш',
+    'Бахрейн',
+    'Белоруссия',
+    'Бельгия',
+    'Болгария',
+    'Босния и Герцеговина',
+    'Великобритания',
+    'Венгрия',
+    'Греция',
+    'Грузия',
+    'Индия',
+    'Индонезия',
+    'Ирак',
+    'Иран',
+    'Ирландия',
+    'Исландия',
+    'Испания',
+    'Казахстан',
+    'Камбоджа',
+    'Катар',
+    'Кипр',
+    'Киргизия',
+    'Китай',
+    'Китайская Республика (Тайвань)',
+    'Лаос',
+    'Латвия',
+    'Ливан',
+    'Литва',
+    'Лихтенштейн',
+    'Малайзия',
+    'Мальдивы',
+    'Мальта',
+    'Молдавия',
+    'Монако',
+    'Монголия',
+    'Польша',
+    'Португалия'
 ]
 
 const mySelectCaretDown = document.querySelector('.my-select-caret-down');
@@ -35,15 +47,18 @@ let mySelectSelectedOption = document.querySelector('.my-select-selected');
 mySelectSelectedOption.innerHTML = '';
 mySelectSelectedOption.innerHTML += array[0];
 
+function displayArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        mySelectOptionsDiv.innerHTML += `<span class="my-select-option">${array[i]}</span>`;
+    }
+}
+
 function onClickMySelectCaretDown(event) {
     mySelectCaretDown.style.display = 'none';
     mySelectCaretUp.style.display = 'block';
 
     mySelectOptionsDiv.innerHTML = '';
-
-    for (let i = 0; i < array.length; i++) {
-        mySelectOptionsDiv.innerHTML += `<span class="my-select-option">${array[i]}</span>`;
-    }
+    displayArray(array);
 }
 
 function onClickMySelectCaretUp(event) {
@@ -60,5 +75,19 @@ function onClickMySelectHead(event) {
     } else {
         mySelectSelectedOption.style.display = 'block';
         mySelectInput.style.display = 'none';
+    }
+}
+
+function filterOptions(event) {
+    let filter = event.target.value.trim(); //TODO: пробелы все равно не удаляются + после перезагрузки страницы остается последнее введенное значение
+    if (filter.length >= 2) {
+        mySelectOptionsDiv.innerHTML = '';
+        let filteredArr = array.filter(function (item) {
+            return item.includes(filter);
+        });
+        displayArray(filteredArr);
+    }
+    if (filter === '') {
+        displayArray(array);
     }
 }
